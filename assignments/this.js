77030239d964edc32111c8521d032c9905b2c3ea
taterntots <matthew.a.weidner@gmail.com>
2019-10-16 14:40:46 -0400

    Described each of the four principles of "this"

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,29 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* In your own words, explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
+* 1. Window Binding - "this" is the global context whenever a function gets invoked without any other rules in place being met. Simply put, "this" would reference the default binding, which is typically the window object when in a browser. 
+
+* 2. Implict Binding - The most common rule used in roughly 80% of cases, implicit binding uses dot notation to determin what "this" references. When the function is being invoked, simply look left of the dot to find the object it's attached to.
+
+* 3. Explicit Binding - With explicit binding, we pass the exact object we wish to refer to as "this" in the first argument of our invoked functions. Unlike implicit binding, we are explicitly specifying what the 'this' keyword is referencing. We use the .call, .apply, and many more methods to help us achieve this.
+
+* 4. New Binding - We use new binding in constructor functions. What new binding does is create brand new objects anytime a funciton is invoked with the 'new' keyword. In other words, the 'this' keyword references the new object being created.
+
+
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 - code example for Window Binding
 
-// code example for Window Binding
 
-// Principle 2
 
-// code example for Implicit Binding
+// Principle 2 - code example for Implicit Binding
 
-// Principle 3
 
-// code example for New Binding
 
-// Principle 4
+// Principle 3 - code example for New Binding
 
-// code example for Explicit Binding
+
+
+// Principle 4 - code example for Explicit Binding
+
